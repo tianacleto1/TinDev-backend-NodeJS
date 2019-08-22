7c79f23d7c9983ff16898db6890d69ad5400afb2
Tiago Anacleto <tianacleto1@gmail.com>
2019-08-22 14:01:05 -0300

    Added model and controller for Dev

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,12 @@
 const express = require('express');
 
 const routes = express.Router();   
+const DevController = require('./controllers/DevController');
 
 routes.get('/', (req, res) => {
     return res.json({ message: `Hello ${req.query.name}` });
 });
 
-routes.post('/devs', (req, res) => {
-
-    return res.json(req.body);
-});
+routes.post('/devs', DevController.store);
 
 module.exports = routes;
