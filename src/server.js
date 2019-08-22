fe0a51a0f78df4d6ec7e3ba1169e6a6dc3c848fc
Tiago Anacleto <tianacleto1@gmail.com>
2019-08-22 13:42:35 -0300

    Added mongoose and connection to mongoDB

--- src/server.js
+++ src/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const routes = require('./routes');
 
 const server = express();
 
-server.use(express.json());
+mongoose.connect('mongodb+srv://anacleto:admin123@cluster0-v1jhz.mongodb.net/oministack8?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+});
 
+server.use(express.json());
 server.use(routes);
 
 server.listen(3333);
