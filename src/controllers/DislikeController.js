006279a475aaa7f009f07593534ef24bc4432962
Tiago Anacleto <tianacleto1@gmail.com>
2019-08-28 13:22:05 -0300

    Implemented dislike between devs

--- src/controllers/DislikeController.js
+++ src/controllers/DislikeController.js
@@ -12,11 +12,7 @@ module.exports = {
             return res.status(400).json({ error: 'Dev doesn`t exists' });
         }
 
-        if (targetDev.likes.includes(loggedDev._id)) {
-            console.log('Deu Match');
-        }
-
-        loggedDev.likes.push(targetDev._id);
+        loggedDev.dislikes.push(targetDev._id);
 
         await loggedDev.save();
 
